90529b20fb4b18306b54dea8551dd97fdc5e2ace
Kusuri Dheeraj Kumar <dheerajkumarkusuri@gmail.com>
2026-02-11 07:20:18 +0530

    Time: 0 ms (100%), Space: 10.1 MB (5.74%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char>st;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='('||s[i]=='['||s[i]=='{'){
-                st.push(s[i]);
-            }
-            else{
-               if(st.empty())return false;
+         stack<char> st;
 
-               if((s[i]==')'&& st.top()!='(') || (s[i]==']' && st.top()!='[')
-                    ||(s[i]=='}' && st.top()!='{')) return false;
-
+        for (char c : s) {
+            if (c == '(') {
+                st.push(')');
+            } else if (c == '{') {
+                st.push('}');
+            } else if (c == '[') {
+                st.push(']');
+            } else {
+                // c is a closing bracket
+                if (st.empty() || st.top() != c) {
+                    return false;
+                }
                 st.pop();
             }
         }
+
         return st.empty();
     }
 };
