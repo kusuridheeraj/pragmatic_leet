20b2880ca03ac7366c994dfaffad3db50140cef6
Kusuri Dheeraj Kumar <dheerajkumarkusuri@gmail.com>
2026-02-15 12:00:03 +0530

    Time: 0 ms (100%), Space: 11.6 MB (97.79%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         int hash[26]={0};
-        for(char c : ransomNote){
-            hash[c-'a']--;
-        }
         for(char c : magazine){
-            hash[ c - 'a']++;
+            hash[c-'a']++;
         }
-        for(int i=0;i<26;i++){
-            if(hash[i]<0){
-                return false;
-            }
+        for(char c : ransomNote){
+            hash[ c - 'a']--;
+                        if(hash[c-'a']<0)return false;
+
         }
         return true;
     }
