7711b4f9a6df1c627a67699ad81fe14737ff4014
Kusuri Dheeraj Kumar <dheerajkumarkusuri@gmail.com>
2026-03-27 09:54:58 +0530

    Time: 16 ms (58.94%), Space: 24.9 MB (86.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mapping;
+
+        for(auto str: strs){
+            string key = str;
+            sort(key.begin(),key.end());
+            mapping[key].push_back(str);
+        }
+
         vector<vector<string>>sol;
-        int n = strs.size();
-        vector<int>v(n,0);
-        for(int i=0;i<n;i++){
-            if(v[i])continue;
-            string s1 = strs[i];
-            vector<string> group;
-            group.push_back(s1);
-            for(int j=i+1;j<n;j++){
-                if(v[j])continue;
-                int charArr[26]={0};
-                string s2 = strs[j];
-                int len = s2.length();
-                bool isAnagram = true;
-                if(i!=j && (s1.length()==s2.length())){
-                    for(int k=0;k<len;k++){
-                        charArr[s1[k]-'a']++;
-                        charArr[s2[k]-'a']--;
-                    }
-                    for( int k =0; k < 26; k++){
-                        if(charArr[k]!=0){
-                            isAnagram = false;
-                        }
-                    }
-                    if(isAnagram){
-                        group.push_back(s2);
-                        v[j]=true;
-                    }
-                }
-            }
+
+        for(auto& [key,group] : mapping){
             sol.push_back(group);
         }
+
         return sol;
     }
 };
